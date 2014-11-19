eb9a152821508b2ea66d00c35e110fcf12e87855
Alexey Geraskin <alexey.geraskin@gmail.com>
2014-11-19 18:29:33 +0100

    NativeArray added

---
+++ FftWrap/NativeArray1D.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FftWrap
+{
+    public class NativeArray1D<T> where T : struct
+    {
+        static readonly int Size = Marshal.SizeOf(typeof(T));
+
+        private readonly int _length;
+        private readonly IntPtr _ptr;
+
+        public NativeArray1D(IntPtr ptr, int length)
+        {
+            _ptr = ptr;
+            _length = length;
+        }
+
+        public int Length
+        {
+            get { return _length; }
+        }
+
+        public T this[int i]
+        {
+            get { return GetValue(i); }
+            set { SetValue(i, value); }
+        }
+
+        private void SetValue(int i, T value)
+        {
+            if (i < 0 || i >= _length)
+                throw new ArgumentOutOfRangeException();
+
+            int shift = i * Size;
+
+            Marshal.StructureToPtr(value, (IntPtr.Add(_ptr, shift)), false);
+        }
+
+        private T GetValue(int i)
+        {
+            if (i < 0 || i >= _length )
+                throw new ArgumentOutOfRangeException();
+
+            // in fortran 2-d indexing is reversed
+            int shift = i * Size;
+
+            return (T)Marshal.PtrToStructure(IntPtr.Add(_ptr, shift), typeof(T));
+        }
+    }
+}
---
+++ FftWrap/NativeArray1DHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace FftWrap
+{
+    public static class NativeArray1DHelper
+    {
+        public static void ForEach<T>(this NativeArray1D<T> array, Action<T> func) where T : struct 
+        {
+            for (int i = 0; i < array.Length; i++)
+                    func(array[i]);
+        }
+
+        public static void ForEach<T>(this NativeArray1D<T> array, Action<int, T> func) where T : struct 
+        {
+            for (int i = 0; i < array.Length; i++)
+                func(i, array[i]);
+        }
+
+        public static void SetEach<T>(this NativeArray1D<T> array, T value) where T : struct 
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = value;
+        }
+    }
+}
